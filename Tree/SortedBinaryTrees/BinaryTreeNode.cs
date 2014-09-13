35a191cab20245866df351c59fa3077148dd15e6
Erik Høyrup Jørgensen <ErikHoyJor@gmail.com>
2014-09-13 22:09:32 +0200

    Added binary trees

---
+++ Tree/SortedBinaryTrees/BinaryTreeNode.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tree.SortedBinaryTrees
+{
+    public class BinaryTreeNode<T> : IDisposable where T : IComparable
+    {
+        private SortedBinaryTree<T> tree;
+
+        public T TValue;
+
+        public BinaryTreeNode<T> Parent;
+        public BinaryTreeNode<T> Left;
+        public BinaryTreeNode<T> Right;
+
+        public bool IsRoot
+        {
+            get { return Parent == null; }
+        }
+        public bool IsLeaf
+        {
+            get { return Left == null && Right == null; }
+        }
+        public bool HasChildren
+        {
+            get { return Left != null || Right != null; }
+        }
+        public bool HasSingleChild
+        {
+            get { return Left != null ^ Right != null; }
+        }
+
+        public BinaryTreeNode(T value, SortedBinaryTree<T> tree, BinaryTreeNode<T> parent)
+        {
+            this.Parent = parent;
+            this.TValue = value;
+            this.tree = tree;
+        }
+
+        public void Insert(T other)
+        {
+            switch (other.CompareTo(this.TValue))
+            {
+                case -1:
+                    if (this.Left != null)
+                        this.Left.Insert(other);
+                    else
+                    {
+                        this.Left = new BinaryTreeNode<T>(other, tree, this);
+                        this.tree.testList.Add(this.Left);
+                    }  
+                    break;
+                case 1:
+                    if (this.Right != null)
+                        this.Right.Insert(other);
+                    else
+                    {
+                        this.Right = new BinaryTreeNode<T>(other, tree, this);
+                        this.tree.testList.Add(this.Right);
+                    }  
+                    break;
+                case 0:
+                default:
+                    break;
+            }
+        }
+
+        public BinaryTreeNode<T> FindMinimum()
+        {
+            if (this.Left != null)
+                return this.Left.FindMinimum();
+            else
+                return this;
+        }
+
+        public BinaryTreeNode<T> FindMaximum()
+        {
+            if (this.Right != null)
+                return this.Right.FindMaximum();
+            else
+                return this;
+        }
+
+        public bool RemoveValue(T item)
+        {
+            switch (item.CompareTo(this.TValue))
+            {
+                case -1:
+                    if (this.Left != null)
+                        return false;
+                    else
+                        return this.Left.RemoveValue(item);
+                case 0:
+                    this.Dispose();
+                    return true;
+                case 1:
+                    if (this.Right != null)
+                        return false;
+                    else
+                        return this.Left.RemoveValue(item);
+                default:
+                    return false;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string print = this.TValue.ToString();
+
+            var temp = this;
+
+            while (!temp.IsRoot)
+            {
+                print += " > " + temp.Parent;
+                temp = temp.Parent;
+            }
+
+            return print;
+        }
+
+        public void Dispose()
+        {
+            if (IsLeaf)
+            {
+                if (!IsRoot)
+                {
+                    if (this.Parent.Left == this)
+                        this.Parent.Left = null;
+
+                    if (this.Parent.Right == this)
+                        this.Parent.Right = null;
+
+                    this.Parent = null;
+                }
+                else
+                {
+                    tree.Root = null;
+                }
+                tree.testList.Remove(this);
+            }
+            else
+            {
+                if (this.Left != null)
+                {
+                    var node = this.Left.FindMaximum();
+                    this.TValue = node.TValue;
+                    node.Dispose();
+                }
+                else if(this.Right != null)
+                {
+                    var node = this.Right.FindMinimum();
+                    this.TValue = node.TValue;
+                    node.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Tree/SortedBinaryTrees/SortedBinaryTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tree.SortedBinaryTrees
+{
+    public class SortedBinaryTree<T> : ICollection<T> where T : IComparable
+    {
+        public List<BinaryTreeNode<T>> testList = new List<BinaryTreeNode<T>>();
+        private int count;
+        private BinaryTreeNode<T> root;
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+        public BinaryTreeNode<T> Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public SortedBinaryTree()
+        {
+            
+        }
+
+        public void Add(T item)
+        {
+            this.count++;
+
+            if (this.root == null)
+                this.root = new BinaryTreeNode<T>(item, this, null);
+
+            this.root.Insert(item);
+        }
+
+        public T Lowest()
+        {
+            return root.FindMinimum().TValue;
+        }
+
+        public T Heighest()
+        {
+            return root.FindMaximum().TValue;
+        }
+
+        public void Clear()
+        {
+            while (this.root != null)
+            {
+                count--;
+                this.root.Dispose();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            this.count--;
+            return this.root.RemoveValue(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Serializable]
+        public class DublicationException : Exception
+        {
+            public DublicationException() { }
+            public DublicationException(string message) : base(message) { }
+            public DublicationException(string message, Exception inner) : base(message, inner) { }
+            protected DublicationException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context)
+                : base(info, context) { }
+        }
+    }
+}
